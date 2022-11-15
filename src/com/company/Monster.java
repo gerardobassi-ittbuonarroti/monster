339af1ca1cf37699064d8f1b8d6636c2da6574f0
gerardo.bassi <gerardo.bassi@buonarroti.tn.it>
2022-11-15 17:11:46 +0100

    feat: metodi attacco e difesa creati

--- src/com/company/Monster.java
+++ src/com/company/Monster.java
@@ -6,7 +6,27 @@ public class Monster {
     private int danno;
 
     Monster(String nome, int puntiferita, int danno){
+        this.nome = nome;
+        this.puntiferita = puntiferita;
+        this.danno = danno;
+    }
 
+    public int attacco(int vita){
+        if(danno>vita){
+            vita = 0;
+        } else{
+            vita = vita-danno;
+        }
+        return vita;
+    }
+
+    public int difesa(int danni){
+        if(puntiferita<danni){
+            puntiferita = 0;
+        } else{
+            puntiferita = puntiferita - danni;
+        }
+        return puntiferita;
     }
 
     
